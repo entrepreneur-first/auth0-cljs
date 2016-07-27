e6906af6efd3a99c18b6f14173daeec5602222ad
David Williams <dpw341@gmail.com>
2016-07-27 14:51:49 +0100

    Switch to using a session cookie instead of local storage, because certain browsers don't support local storage in private browsing mode

--- src/auth0_cljs/user.cljs
+++ src/auth0_cljs/user.cljs
@@ -1,18 +1,19 @@
-(ns auth0-cljs.user)
+(ns auth0-cljs.user
+  (:import goog.net.Cookies))
 
 (def logged-in-user (atom {}))
 
 (defn get-user-token
   "Gets the Auth0 user token from local storage"
   []
-  (.getItem js/localStorage "userToken"))
+  (.get (goog.net.Cookies. js/document) "auth0-cljs-user-token"))
 
 (defn set-user-token!
   "Sets the Auth0 user token in local storage"
   [token]
-  (.setItem js/localStorage "userToken" token))
+  (.set (goog.net.Cookies. js/document) "auth0-cljs-user-token" token -1 "/"))
 
 (defn clear-user-token!
   "Clears the Auth0 user token (e.g. for logout)"
   []
-  (.removeItem js/localStorage "userToken"))
+  (.set (goog.net.Cookies. js/document) "auth0-cljs-user-token" "" 0 "/"))
